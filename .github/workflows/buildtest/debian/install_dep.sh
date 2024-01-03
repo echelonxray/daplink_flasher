c9b967d2b85e186f131e204f64062b88ad70dbfa
Michael T. Kloos <michael@michaelkloos.com>
2024-01-03 13:00:55 -0500

    Improve script commands.  Split update to separate command.

--- .github/workflows/buildtest/debian/install_dep.sh
+++ .github/workflows/buildtest/debian/install_dep.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-apt update
-apt upgrade
-apt install build-essential
-apt install libusb-1.0-0-dev
+apt-get -y -q install build-essential
+apt-get -y -q install libusb-1.0-0-dev
 
 exit 0
---
+++ .github/workflows/buildtest/debian/update.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e
+
+apt-get -y -q update
+apt-get -y -q upgrade
+
+exit 0
